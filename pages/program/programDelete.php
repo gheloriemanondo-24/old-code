7b5d494b53c798dee023e6f17c253e0922cc5fda
Ghelorie Manondo <ghelorie.manondo.24@usjr.edu.ph>
2026-05-19 14:39:35 +0800

    initial commit

--- pages/program/programDelete.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db = getDB();
-$id = $_GET['id'] ?? '';
-if ($id) {
-    try { $db->prepare("DELETE FROM programs WHERE progid=?")->execute([$id]);
-          header('Location: LogIn.php?section=program&page=programList&msg=deleted');
-    } catch (PDOException $e) { header('Location: LogIn.php?section=program&page=programList&msg=error'); }
-} else { header('Location: LogIn.php?section=program&page=programList'); }
-exit;
